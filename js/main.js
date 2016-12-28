3c26def6c0e386eb06f3696669362134533ec1a6
jboltz88 <boltz.justin@gmail.com>
2016-12-28 15:50:50 -0600

    Change blocmetrics tracking from local to heroku

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ var blocmetrics = {};
 blocmetrics.report = function(eventName) {
    var event = {event: { name: eventName }};
    var request = new XMLHttpRequest();
-   request.open("POST", "http://localhost:4000/api/events", true);
+   request.open("POST", "http://blocmetrics-boltz.herokuapp.com/api/events", true);
    request.setRequestHeader('Content-Type', 'application/json');
    request.send(JSON.stringify(event));
 }
